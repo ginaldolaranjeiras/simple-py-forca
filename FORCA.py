7fea138b7f78ea5cbc7962f83cfe51d072acbc23
Ginaldo Laranjeiras <50729163+ginaldolaranjeiras@users.noreply.github.com>
2020-05-19 04:45:02 -0300

    Simplificando o código
    Remoção de funções desnecessárias. Síntese para programação em cascata.

--- FORCA.py
+++ FORCA.py
@@ -1,34 +1,19 @@
-from os import path
-import random
-
-def palavraObtida():
-    arquivo = open('Lista_de_Palavras.txt', 'r')
-    global palavra
-    if arquivo.mode == 'r':
-
-        linhas_do_arquivo = arquivo.readlines()
-        for linha in linhas_do_arquivo:
-            palavra = linhas_do_arquivo[random.randrange(len(linhas_do_arquivo))]
-            palavra = palavra.upper()
-
-    arquivo.close()
-
-
-def palavraOculta():
-    palavraObtida()
-    obscure = "__ "
-    global obscureword
-    obscureword = []
-
-    for letra in range(len(palavra)-1):
-        obscureword.append(obscure)
-
-    return obscureword
-
-
-palavraOculta()
 vidas = 5
 
+arquivo = open('Lista_de_Palavras.txt', 'r')
+if arquivo.mode == 'r':
+    linhas_do_arquivo = arquivo.readlines()
+    for linha in linhas_do_arquivo:
+        palavra = linhas_do_arquivo[random.randrange(len(linhas_do_arquivo))]
+        palavra = palavra.upper()
+arquivo.close()
+
+obscure = "_ "
+
+obscureword = []
+
+for letra in range(len(palavra)-1):
+    obscureword.append(obscure)
 
 while vidas > 0: 
     for space in obscureword:
@@ -41,17 +26,14 @@ while vidas > 0:
 
 
     for i in range(len(palavra)):
-
         if palavra[i] == letra:
             obscureword[i] = letra
-
 
     for space in obscureword:
         print(space, end='')
 
     print('')
     letraspalavra = palavra.count(letra)
-
 
     if letraspalavra == 0:
         print('Você errou a letra e perdeu uma vida')
@@ -61,11 +43,9 @@ while vidas > 0:
 
     if vidas > 0:
         print("Você ainda tem", vidas, "vidas")
-
     else:
         print('Você perdeu! :(')
         print('A palavra era', palavra)
-
 
     if obscureword == list(palavra[:-1]):
         print('Você venceu!')
